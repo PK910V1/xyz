a699d83c903539628e711f7659ad91638de3d839
unknown <info.learn2flys@gmail.com>
2020-01-26 22:39:13 +0700

    make it better

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 access_token = "y3B+LvzkDZ0GIDpKtAmXJM6kZEXlVzRZLgBG6AgofFv9NvsJuonCR/1Wk5vlkaLZrmk93Jq8Kiva+8w6qekY22MmbK/7h+PSXokIR4DxBlRZbZQHO9EOrBezCjLh+4CDLoG2yZUUAdqOcMt+NMFHcwdB04t89/1O/w1cDnyilFU="
 channel_secret = "8a29237583efd64fc1af19221992d8a3"
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-CRV_PATH = os.path.join("basic_python","CSVs")
+CSV_PATH = os.path.join("basic_python","CSVs")
